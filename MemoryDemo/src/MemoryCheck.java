9f67425443e89cda73c46afe96a1b9350ba7b57b
Amit <Amit@DESKTOP-GINF7JH>
2019-01-02 01:38:53 +0530

    Adding files

---
+++ MemoryDemo/src/MemoryCheck.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MemoryCheck {
+
+	public static void main(String[] args) throws Exception {
+		
+	}
+
+}
